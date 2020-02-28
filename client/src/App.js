0ed4fdb769da8b71201fbbd6803aa722f49d0f2e
pravingoswami <goswami4737@gmail.com>
2020-02-28 20:01:02 +0530

    setup for chat app frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './bootstrap.css'
+import Chat from './Chat'
+
+function App() {
+  return (
+    <div >
+        <Chat />
+    </div>
+  );
+}
+
+export default App;
